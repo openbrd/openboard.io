856350e3233c5b56a23af79ba3df904808387009
Airead Fan <fanrenhao@crazyhornets.com>
2014-05-12 11:42:18 +0800

    dynamic load model module

--- flaskApp/model/__init__.py
+++ flaskApp/model/__init__.py
@@ -1,5 +1,16 @@
-import db_declare
-import user
-import record
+import os
+import importlib
 
-db = db_declare.db
+cur_path = os.path.dirname(os.path.realpath(__file__))
+
+modules = {}
+
+for dir_path, dir_name, filenames in os.walk(cur_path):
+    for filename in filenames:
+        name, ext = os.path.splitext(filename)
+        if ext == '.py':
+            moduleName = 'model.' + name
+            modules[name] = importlib.import_module(moduleName)
+
+
+db = modules['db_declare'].db
